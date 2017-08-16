8621ffa87f3ed40c5f6ceb19c2c274f1a88b1aae
Anthony Dahanne <anthony.dahanne@gmail.com>
2017-08-16 10:43:08 -0400

    Generate project with start.spring.io, and cache

---
+++ src/main/java/org/terracotta/ehcache3diskpersistencedemo/Ehcache3DiskPersistenceDemoApplication.java
@@ -0,0 +1,12 @@
+package org.terracotta.ehcache3diskpersistencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ehcache3DiskPersistenceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ehcache3DiskPersistenceDemoApplication.class, args);
+	}
+}
